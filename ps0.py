6ec9a7adbb6b66523efa9b8009dbef7a1bf2ab6b
shuaybkug <54536632+shuaybkug@users.noreply.github.com>
2019-08-26 21:00:08 +0100

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,9 @@
+
+
+import math
+x = input("Enter a number x: ")
+y = input("Enter a number y: ")
+x_root_y = int(x)**int(y)
+print("x**y = " + str(x_root_y))
+print("log(x) = " + str(math.log(int(x), 2)))
+
